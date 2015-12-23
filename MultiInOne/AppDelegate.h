353fb351fa6bce973d092825dcf9ee37471dbdb1
Ray Lee <raylee@e-lead.com.tw>
2015-12-23 20:29:17 +0800

    Initial Commit

---
+++ MultiInOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiInOne
+//
+//  Created by Ray on 2015/12/23.
+//  Copyright © 2015年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiInOne/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MultiInOne
+//
+//  Created by Ray on 2015/12/23.
+//  Copyright © 2015年 Ray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
